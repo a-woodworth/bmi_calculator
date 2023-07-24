d7178ac97fd26fc6efa41f9bf185e0839524f7e4
Amy Woodworth <woodworthcommunications@gmail.com>
2023-07-24 18:48:30 -0500

    Work on js for metric bmi calculation.

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const bmiForm = document.forms['calculator'];
-const radioBtns = [...bmiForm.elements.measurement];
+const radioBtns = bmiForm.elements.measurement;
+const metricHeight = bmiForm.elements.metricHeight;
+const metricWeight = bmiForm.elements.metricWeight;
+const imperialFeet = bmiForm.elements.imperialHeightFt;
+const imperialInches = bmiForm.elements.imperialHeightInch;
+const imperialStones = bmiForm.elements.imperialWeightStone;
+const imperialPounds = bmiForm.elements.imperialWeightPound;
+const bmi = bmiForm.elements.bmi;
+const lowBMI = 18.5;
+const highBMI = 24.9;
+let height = 0;
+let weight = 0;
 
 function toggleMeasurment(value) {
   const imperialBlocks = document.querySelectorAll('.imperial-units');
@@ -18,6 +29,26 @@ function toggleMeasurment(value) {
   }
 }
 
+// CDC - How is BMI calculated: 
+// https://www.cdc.gov/healthyweight/assessing/bmi/adult_bmi/index.html#Interpreted
+function calculateMetricBMI(height, weight) {
+  // Calculate metric bmi value
+  const metricScore = ( weight / Math.pow( (height / 100), 2) ).toFixed(1);
+
+  // Calculate metric weight range values
+  const metricLow = ( [lowBMI * Math.pow(height, 2)] / 10000 ).toFixed(1);
+  const metricHigh = ( [highBMI * Math.pow(height, 2)] / 10000 ).toFixed(1);
+
+  // Create text for metric range
+  const metricRange = `${metricLow}kgs - ${metricHigh}kgs`;
+
+  return [metricScore, metricRange];
+}
+
+function calculateImperialBMI(height, weight) {
+  return ( (weight / Math.pow(height, 2)) * 703 ).toFixed(1);
+}
+
 radioBtns.forEach(radio => {
   radio.addEventListener('change', (e) => {
     let unitValue = e.target.value;
@@ -25,3 +56,8 @@ radioBtns.forEach(radio => {
     toggleMeasurment(unitValue);
   });
 });
+
+// Note: 1 stone = 14 pounds
+
+// Note: Need to add to text output
+// aria-live="polite" aria-atomic="true" 
